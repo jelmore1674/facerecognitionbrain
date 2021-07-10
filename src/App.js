419c5753fd032710b741f4b39b6d6a312135b477
Justin Elmore <jelmore1674@pm.me>
2021-07-10 14:32:44 -0500

    added the ability to detect mulitple faces

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends React.Component {
 		this.state = {
 			input: '',
 			imageUrl: '',
-			box: {},
+			boxes: [],
 			route: 'signin',
 			isSignedIn: false,
 			user: {
@@ -145,21 +145,22 @@ class App extends React.Component {
 	};
 
 	calculateFaceLocation = (data) => {
-		const clarifaiFace =
-			data.outputs[0].data.regions[0].region_info.bounding_box;
 		const image = document.getElementById('inputimage');
 		const width = Number(image.width);
 		const height = Number(image.height);
-		return {
-			leftCol: clarifaiFace.left_col * width,
-			topRow: clarifaiFace.top_row * height,
-			rightCol: width - clarifaiFace.right_col * width,
-			bottomRow: height - clarifaiFace.bottom_row * height,
-		};
+		return data.outputs[0].data.regions.map((face) => {
+			const clarifaiFace = face.region_info.bounding_box;
+			return {
+				leftCol: clarifaiFace.left_col * width,
+				topRow: clarifaiFace.top_row * height,
+				rightCol: width - clarifaiFace.right_col * width,
+				bottomRow: height - clarifaiFace.bottom_row * height,
+			};
+		});
 	};
 
-	displayFaceBox = (box) => {
-		this.setState({ box: box });
+	displayFaceBox = (boxes) => {
+		this.setState({ boxes: boxes });
 	};
 
 	onInputChange = (event) => {
@@ -211,7 +212,7 @@ class App extends React.Component {
 	};
 
 	render() {
-		const { isSignedIn, imageUrl, route, box } = this.state;
+		const { isSignedIn, imageUrl, route, boxes } = this.state;
 		return (
 			<div className='App'>
 				<Particles
@@ -219,23 +220,23 @@ class App extends React.Component {
 					init={this.particlesInit}
 					loaded={this.particlesLoaded}
 					options={this.options}
-				/>
+				/>{' '}
 				<Navigation
 					isSignedIn={isSignedIn}
 					onRouteChange={this.onRouteChange}
-				/>
+				/>{' '}
 				{route === 'home' ? (
 					<div>
 						<Logo />
 						<Rank
 							name={this.state.user.name}
 							entries={this.state.user.entries}
-						/>
+						/>{' '}
 						<ImageLinkForm
 							onInputChange={this.onInputChange}
 							onButtonSubmit={this.onButtonSubmit}
-						/>
-						<FaceRecognition box={box} imageUrl={imageUrl} />
+						/>{' '}
+						<FaceRecognition boxes={boxes} imageUrl={imageUrl} />{' '}
 					</div>
 				) : route === 'signin' ? (
 					<SignIn
@@ -247,7 +248,7 @@ class App extends React.Component {
 						loadUser={this.loadUser}
 						onRouteChange={this.onRouteChange}
 					/>
-				)}
+				)}{' '}
 			</div>
 		);
 	}
